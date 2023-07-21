159ca2b78e451513b274c68c15afd1d8525b3bd6
Karem1-Atef1 <karemkmbsh6@gmail.com>
2023-07-21 02:14:21 -0700

    1.isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _isdigit - check if c is digit.
+ *
+ * @c: input for alphabet
+ *
+ * Return: 1 if its digit. 0 if not
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+return (1);
+else
+return (0);
+}
